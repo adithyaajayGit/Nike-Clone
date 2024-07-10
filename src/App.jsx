7839b6f6a2e775a8a85269b7e00395c5dac18df9
Adithya Ajay <adithyaajay098@gmail.com>
2024-07-10 22:40:30 +0530

    add:icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { CustomerReviews,Footer,Hero,PopularProduct,Services,SpecialOffer,Subscribe,SuperQuality } from "./sections";
+import Nav from "./components/Nav";
 const App = () =>(
     <main className="relative">
-        {/* <nav /> */}
+        <Nav />
         <section className="xl:padding-1 wide:padding-r padding-b">
           <Hero />
         </section>
