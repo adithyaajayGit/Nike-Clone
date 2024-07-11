c1b99b2be29d249357c96cbf88ac3f0448a64c0e
Adithya Ajay <adithyaajay098@gmail.com>
2024-07-11 23:40:02 +0530

    style:hero-span

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -12,9 +12,9 @@ const Hero = () => {
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span>The New Arrival</span>
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
           <br />
-          <span>Nike </span> Shoes
+          <span className="text-coral-red inline-block mt-3">Nike </span> Shoes
         </h1>
         <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
         <Button
