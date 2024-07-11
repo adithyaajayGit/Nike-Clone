6af05741ca8f0f3eec2f307b25046dd949eb041b
Adithya Ajay <adithyaajay098@gmail.com>
2024-07-12 00:32:15 +0530

    add:shoeimage

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,14 +1,14 @@
-import { arrowRight } from "../assets/icons"
-import Button from "../components/Button"
-import { statistics } from "../constants"
+import { arrowRight } from "../assets/icons";
+import { bigShoe1 } from "../assets/images";
+import Button from "../components/Button";
+import { statistics } from "../constants";
 
 const Hero = () => {
   return (
     <section
-    id="home"
-    className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
@@ -16,24 +16,36 @@ const Hero = () => {
           <br />
           <span className="text-coral-red inline-block mt-3">Nike </span> Shoes
         </h1>
-        <p className="font-montserrat text-slate-gray text-lg mt-6 leading-8 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
-        <Button
-         label="shop now" 
-         iconURL={arrowRight}
-        />
+        <p className="font-montserrat text-slate-gray text-lg mt-6 leading-8 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovation for your active life.
+        </p>
+        <Button label="shop now" iconURL={arrowRight} />
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-          {statistics.map((stat)=>(
-              <div key={stat.label}>
-                <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
-                <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
-              </div>
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+            </div>
           ))}
-
         </div>
       </div>
 
-    </section>
-  )
-}
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoe1}
+          alt="shoe collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+      </div>
+      
+      <div>
 
-export default Hero
+      </div>
+
+    </section>
+  );
+};
+
+export default Hero;
