12b7e8b88b3a527a218a789d2341b523d5fbf637
Adithya Ajay <adithyaajay098@gmail.com>
2024-07-11 23:23:47 +0530

    style:button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({label, iconURL}) => {
   return (
-    <button className=''>
+    <button className='flex justify-center items-center px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red '>
         {label}
         <img src={iconURL}
          alt="arrow right"
