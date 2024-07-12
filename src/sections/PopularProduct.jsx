c225e54e4a8cba213d62cffec35674ef8ab5fd60
Adithya Ajay <adithyaajay098@gmail.com>
2024-07-12 23:40:27 +0530

    add:popularproducts

--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -1,9 +1,13 @@
+import { products } from "../constants"
 
 const PopularProduct = () => {
   return (
-    <div>
-      
-    </div>
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5 ">
+        <h2>Popular Products</h2>
+        <p>Experience top-notch quality and style with our sought after selections discover a world of comfort,design and values</p>
+      </div>
+    </section>
   )
 }
 
