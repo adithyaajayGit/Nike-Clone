c684931e93831106f9a9ff1a3a830dccdd653076
Adithya Ajay <adithyaajay098@gmail.com>
2024-07-12 21:46:41 +0530

    add:changeimage

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const ShoeCard = () => {
+
+const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImage}) => {
+  const handleClick = () =>{
+    if(bigShoeImage !== imgURL.bigShoe){
+      changeBigShoeImage(imgURL.bigShoe)
+    }
+  }
+  
   return (
-    <div>
-      
+    <div className={`border-2 rounded-xl 
+      ${bigShoeImage === imgURL
+        ? 'border-coral-red'
+        : 'border-transparent'
+      } cursor-pointer max-sm:flex-1
+    
+    `}
+    onClick={handleClick}
+    
+    > 
     </div>
   )
 }
