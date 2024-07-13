d222c558c1f18cd46980ca96a43d4bd599009ddc
Adithya Ajay <adithyaajay098@gmail.com>
2024-07-13 22:45:55 +0530

    style:text

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -18,7 +18,7 @@ const SpecialOffer = () => {
         <p className='mt-4 lg:max-w-lg info-text'>
         Ensuring premium comfort and style,our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and  a touch of elegance
         </p>
-
+    <p className="mt-6 lg:max-w-lg">Our dedication to detail and excellance ensures your satisfaction</p>
         <Button label='View details' />
 
     
