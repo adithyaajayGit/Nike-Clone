ec749794997788b0f9de2889933b45ff098aee53
Adithya Ajay <adithyaajay098@gmail.com>
2024-07-13 21:43:20 +0530

    add:superproduct-text

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button"
 
 const SpecialOffer = () => {
   return (
@@ -7,7 +8,20 @@ const SpecialOffer = () => {
    >
 
     <div className="flex flex-1 flex-col">
-      
+ 
+
+        <h2 className='font-palanquin text-4xl capitalize max-sm:text-[72px] font-bold lg:max-w-lg'>
+          Wr Provide You
+          <span className='text-coral-red '> Super</span>
+          <span className='text-coral-red '> Quality</span> Shoes
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+        Ensuring premium comfort and style,our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and  a touch of elegance
+        </p>
+
+        <Button label='View details' />
+
+    
     </div>
 
    </section>
