e4d7d7d3508baabe6f9b4d81148ba17a2d44f73b
Adithya Ajay <adithyaajay098@gmail.com>
2024-07-14 00:53:42 +0530

    add:ReviewCard

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,3 +1,4 @@
+import ReviewCard from "../components/ReviewCard"
 import { reviews } from "../constants"
 
 const CustomerReviews = () => {
@@ -11,8 +12,11 @@ const CustomerReviews = () => {
       <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
         {reviews.map((review) => (
           <ReviewCard
-          
-          
+          key={review.customerName}
+          imgURL={review.imgURL}
+          customerName={review.customerName}
+          rating={review.rating}
+          feedback={review.feedback}
           />
         ))}
       </div>
