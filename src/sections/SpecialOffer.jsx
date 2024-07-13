b5c0f10538ec68bdaf59ed8e91e4989c3db6c179
Adithya Ajay <adithyaajay098@gmail.com>
2024-07-13 22:51:52 +0530

    add:superquality-image

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,32 +1,8 @@
-import Button from "../components/Button"
+
 
 const SpecialOffer = () => {
   return (
-   <section
-   id="about-us"
-   className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container" 
-   >
-
-    <div className="flex flex-1 flex-col">
- 
-
-        <h2 className='font-palanquin text-4xl capitalize max-sm:text-[72px] font-bold lg:max-w-lg'>
-          Wr Provide You
-          <span className='text-coral-red '> Super</span>
-          <span className='text-coral-red '> Quality</span> Shoes
-        </h2>
-        <p className='mt-4 lg:max-w-lg info-text'>
-        Ensuring premium comfort and style,our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and  a touch of elegance
-        </p>
-    <p className="mt-6 lg:max-w-lg">Our dedication to detail and excellance ensures your satisfaction</p>
-    <div className="mt-11">
-    <Button label='View details' />
-    </div>
-
-    
-    </div>
-
-   </section>
+ <div></div>
   )
 }
 
