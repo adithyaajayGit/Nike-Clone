3545b91d6b50d178c898650e0313d1e5b5b478d7
Adithya Ajay <adithyaajay098@gmail.com>
2024-07-14 14:50:08 +0530

    add:copyright

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,4 @@
+import { copyrightSign } from "../assets/icons"
 import { footerLogo } from "../assets/images"
 import { footerLinks, socialMedia } from "../constants"
 
@@ -46,8 +47,19 @@ const Footer = () => {
               ))}
         </div>
       </div>
-      <div className="">
-
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+                <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+                  <img src={copyrightSign}
+                   alt="copy"
+                   width={20}
+                   height={20}
+                   className="rounded-full m-0"
+                   
+                   />
+                   <p>Copyright. All rights reserved</p>
+                   
+                </div>
+                <p className="font-montserrat cursor-pointer">Terms and Conditions</p>
       </div>
     </footer>
   )
